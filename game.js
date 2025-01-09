5a43242b688cf96dfa935e21c21d9f5c982bcf27
Birgit Võsar <birgit.vosar@gmail.com>
2025-01-09 17:00:06 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,24 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+
+  $(".btn").click(function(){
+    var userChosenColour = this.id;
+    console.log(userChosenColour);
+  }
+);
+
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+nextSequence();
